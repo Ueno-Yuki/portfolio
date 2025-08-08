18982d61ca68f76236f959e67b9183700637d5f8
yuki_ueno <jam14_01@icloud.com>
2025-08-08 18:37:24 +0900

    Vercelビルドエラー

--- next.config.ts
+++ next.config.ts
@@ -1,4 +1,5 @@
 import type { NextConfig } from "next";
+import path from "path";
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
@@ -72,6 +73,12 @@ const nextConfig: NextConfig = {
       // 必要に応じてリダイレクト設定を追加
     ];
   },
+  webpack: config => {
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
+    config.resolve.alias["~"] = path.join(__dirname, "src");
+    return config;
+  },
 };
+
 
 export default nextConfig;
