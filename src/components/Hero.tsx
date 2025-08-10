2637c9b1c72a258569ab2c6070ae9f2cbc8d9bf2
yuki_ueno <jam14_01@icloud.com>
2025-08-10 15:05:12 +0900

    サブタイトルのCSS修正

--- src/components/Hero.tsx
+++ src/components/Hero.tsx
@@ -6,7 +6,7 @@ export default function Hero() {
   return (
     <div className={styles.heroSection}>
       <h1 className={`${commonStyles.titlePrimary} ${styles.heroTitle} glow`}>{HERO.title}</h1>
-      <h2 className={`${commonStyles.titleSecondary} ${styles.heroSubtitle} glow_min`}>{HERO.subtitle}</h2>
+      <h2 className={`${styles.heroSubtitle}`}>{HERO.subtitle}</h2>
       <div className={styles.matrix}>
         <div className={styles.matrixCode}>{HERO.matrixCode}</div>
       </div>
